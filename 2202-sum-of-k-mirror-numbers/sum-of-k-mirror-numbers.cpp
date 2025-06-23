0353de8942e5c0ff58808664b5a4cb9926684d4a
APOORAV RAJ <114277411+apooravraj@users.noreply.github.com>
2025-06-23 14:54:50 +0530

    Time: 2907 ms (5.26%) | Memory: 88.7 MB (27.19%) - LeetSync

---
+++ 2202-sum-of-k-mirror-numbers/sum-of-k-mirror-numbers.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+
+bool isPalindrome(string basek) {
+    int i = 0;
+    int j = basek.length()-1;
+
+    while(i <= j) {
+        if(basek[i] != basek[j]) {
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+
+}
+
+string convertToBasek(long long num, int k) {
+    if(num == 0) {
+        return "0";
+    }
+    string res = "";
+    while(num > 0) {
+        res += to_string(num%k);
+        num/= k;
+    }
+    return res;
+}
+    long long kMirror(int k, int n) {
+        long long sum = 0;
+        int L = 1;
+
+        while(n>0){
+            int half_length = (L+1)/2;
+
+            long long min_num = pow(10, half_length-1);
+            long long max_num = pow(10, half_length) - 1;
+
+            for(int num = min_num; num <= max_num; num++) {
+                string first_half = to_string(num);
+                string second_half = first_half;
+                reverse(begin(second_half), end(second_half));
+
+                string pal = "";
+                if(L % 2 == 0) {
+                    pal = first_half + second_half;
+
+                } else {
+                    pal = first_half + second_half.substr(1);
+                }
+                long long pal_num = stoll(pal);
+                string basek = convertToBasek(pal_num, k);
+
+                if(isPalindrome(basek)) {
+                    sum += pal_num;
+                    n--;
+                    if(n == 0) {
+                        break;
+                    }
+                }
+
+            }
+            L++;
+        }
+        return sum;
+
+        
+    }
+};
