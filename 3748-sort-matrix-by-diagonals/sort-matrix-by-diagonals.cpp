940dbd0a6d289798442389ed96c0f452ea05383c
APOORAV RAJ <114277411+apooravraj@users.noreply.github.com>
2025-08-28 22:23:05 +0530

    Time: 7 ms (73.05%) | Memory: 46.4 MB (11.73%) - LeetSync

---
+++ 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+
+        unordered_map<int , vector<int>> mp;
+
+        // Store elements by diagonals
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                int diag = i - j;
+                mp[diag].push_back(grid[i][j]);
+            }
+        }
+
+        // Sort each diagonal
+        for(auto &it : mp) {
+            if(it.first >= 0) {
+                sort(it.second.begin(), it.second.end());
+            } else {
+                sort(it.second.rbegin(), it.second.rend());
+            }
+        }
+
+        // Refill grid
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                int diag = i - j;
+                grid[i][j] = mp[diag].back();
+                mp[diag].pop_back();
+            }
+        }
+
+        return grid;
+    }
+};
