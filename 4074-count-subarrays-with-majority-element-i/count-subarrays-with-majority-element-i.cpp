9be9dd245d87b363f29ee4a1c49c92ea7f5ef8cc
APOORAV RAJ <114277411+apooravraj@users.noreply.github.com>
2026-06-25 11:21:56 +0530

    Time: 0 ms (100.00%) | Memory: 39.7 MB (57.52%) - LeetSync

--- 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.cpp
+++ 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.cpp
@@ -1,19 +1,22 @@
+constexpr int N=2002;
+int cntB[N], bias;
 class Solution {
 public:
     static int countMajoritySubarrays(vector<int>& nums, int target) {
-        int n = nums.size(), balance = 0;
-        unordered_map<int, int> cntB;
-        cntB.reserve(n);
-        cntB[balance] = 1;
-        int ans = 0, sum = 0;
-
-        for(int x : nums) {
-            if(x==target) sum+=cntB[balance++];
+        bias=nums.size()+1;
+        int balance=bias;
+        memset(cntB , 0, (2*bias)*sizeof(int));
+        cntB[balance]=1;
+        int ans=0, sum=0;
+        
+        for(int x: nums) {
+            if (x==target) sum+=cntB[balance++];
             else sum-=cntB[--balance];
 
             cntB[balance]++;
             ans+=sum;
         }
+        
         return ans;
     }
 };
